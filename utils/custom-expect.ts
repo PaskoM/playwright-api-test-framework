7f4880ab4f8a965a947cdd8eb0eef4fa9b9df8ec
Mariola Pasko <paskomariola@gmail.com>
2025-12-03 21:49:15 +0000

    added custom assertion shouldMatchSchema

--- utils/custom-expect.ts
+++ utils/custom-expect.ts
@@ -1,5 +1,6 @@
 import { expect as baseExpect } from "@playwright/test";
 import { APILogger } from "./logger";
+import { validateSchema } from "./schema-validator";
 
 let apiLogger: APILogger;
 
@@ -12,6 +13,7 @@ declare global {
     interface Matchers<R, T> {
       shouldEqual(expected: T): R;
       shouldBeLessThanOrEqual(expected: T): R;
+      shouldMatchSchema(dirName: string, fileName: string): Promise<R>;
     }
   }
 }
@@ -69,6 +71,24 @@ export const expect = baseExpect.extend({
       `Received: ${this.utils.printReceived(received)}\n\n` +
       `Recent API Activity: \n ${logs}`;
 
+    return {
+      message: () => message,
+      pass,
+    };
+  },
+
+  async shouldMatchSchema(received: any, dirName: string, fileName: string) {
+    let pass: boolean;
+    let message: string = "";
+    try {
+      await validateSchema(dirName, fileName, received);
+      pass = true;
+      message = "Schema ";
+    } catch (e: any) {
+      pass = false;
+      const logs = apiLogger.getRecentLogs();
+      message = `${e.message}\n\nRecent API activity: \n${logs}`;
+    }
     return {
       message: () => message,
       pass,
